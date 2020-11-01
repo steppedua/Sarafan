dca5e2866ee3a07437e977a427ba2f4f900e6cd6
Stepanyan Eduard <stepanyan.edua@gmail.com>
2020-11-01 13:40:53 +0300

    Add oAuth2 authorization

---
+++ src/main/java/com/steppedua/sarafan/domain/User.java
@@ -0,0 +1,29 @@
+package com.steppedua.sarafan.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ussr")
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -3647197254860304641L;
+
+    @Id
+    private String id;
+    private String name;
+    private String userpic;
+    private String email;
+    private String gender;
+    private String locale;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime lastVisit;
+}
--- src/main/java/com/steppedua/sarafan/repository/UserDetailsRepository.java
+++ src/main/java/com/steppedua/sarafan/repository/UserDetailsRepository.java
@@ -1,9 +1,9 @@
 package com.steppedua.sarafan.repository;
 
-import com.steppedua.sarafan.domain.Message;
+import com.steppedua.sarafan.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
+public interface UserDetailsRepository extends JpaRepository<User, String> {
 }
