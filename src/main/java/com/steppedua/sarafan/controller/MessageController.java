5f147674de86224b4c00d7df1f94780c1e28b72a
Stepanyan Eduard <stepanyan.edua@gmail.com>
2020-11-03 20:16:21 +0300

    Add Websocket

--- src/main/java/com/steppedua/sarafan/controller/MessageController.java
+++ src/main/java/com/steppedua/sarafan/controller/MessageController.java
@@ -6,6 +6,8 @@ import com.steppedua.sarafan.domain.Views;
 import com.steppedua.sarafan.repository.MessageRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -53,6 +55,12 @@ public class MessageController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Message message) {
         messageRepository.delete(message);
+    }
+
+    @MessageMapping("/changeMessage")
+    @SendTo("/topic/activity")
+    public Message message(Message message) {
+        return messageRepository.save(message);
     }
 
 }
