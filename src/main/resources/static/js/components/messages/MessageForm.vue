d3cd559d05aa1a8f735ec7b493bee6cf0f974026
Stepanyan Eduard <stepanyan.edua@gmail.com>
2020-11-04 18:13:10 +0300

    Add Vuex for storing state

--- src/main/resources/static/js/components/messages/MessageForm.vue
+++ src/main/resources/static/js/components/messages/MessageForm.vue
@@ -12,56 +12,38 @@
 </template>
 
 <script>
-import messagesApi from 'api/messages'
-
-export default {
-  props: ['messages', 'messageAttr'],
-  data() {
-    return {
-      text: '',
-      id: ''
+    import { mapActions } from 'vuex'
+    export default {
+        props: ['messageAttr'],
+        data() {
+            return {
+                text: '',
+                id: ''
+            }
+        },
+        watch: {
+            messageAttr(newVal, oldVal) {
+                this.text = newVal.text
+                this.id = newVal.id
+            }
+        },
+        methods: {
+            ...mapActions(['addMessageAction', 'updateMessageAction']),
+            save() {
+                const message = {
+                    id: this.id,
+                    text: this.text
+                }
+                if (this.id) {
+                    this.updateMessageAction(message)
+                } else {
+                    this.addMessageAction(message)
+                }
+                this.text = ''
+                this.id = ''
+            }
+        }
     }
-  },
-  watch: {
-    messageAttr(newVal, oldVal) {
-      this.text = newVal.text
-      this.id = newVal.id
-    }
-  },
-  methods: {
-    save() {
-
-      const message = {
-        id: this.id,
-        text: this.text
-      }
-
-      if (this.id) {
-        messagesApi.update(message).then(result =>
-            result.json().then(data => {
-              const index = this.messages.findIndex(item => item.id === data.id)
-              this.messages.splice(index, 1, data)
-            })
-        )
-      } else {
-        messagesApi.add(message).then(result =>
-            result.json().then(data => {
-              const index = this.messages.findIndex(item => item.id === data.id)
-
-              if (index > -1) {
-                this.messages.splice(index, 1, data)
-              } else {
-                this.messages.push(data)
-              }
-            })
-        )
-      }
-
-      this.text = ''
-      this.id = ''
-    }
-  }
-}
 </script>
 
 <style>
--- src/main/resources/static/js/components/messages/MessageList.vue
+++ src/main/resources/static/js/components/messages/MessageList.vue
@@ -1,22 +1,19 @@
 <template>
   <v-layout align-space-around justify-start column>
-    <message-form :messages="messages" :messageAttr="message"/>
-    <message-row v-for="message in sortedMessages"
-                 :key="message.id"
+    <message-form :messageAttr="message"/>
+    <message-row v-for="(message,index) in sortedMessages"
+                 :key="`message.id-${index}`"
                  :message="message"
-                 :editMessage="editMessage"
-                 :deleteMessage="deleteMessage"
-                 :messages="messages"/>
+                 :editMessage="editMessage"/>
   </v-layout>
 </template>
 
 <script>
-import MessageRow from "components/messages/MessageRow.vue"
+import {mapGetters} from 'vuex'
+import MessageRow from 'components/messages/MessageRow.vue'
 import MessageForm from 'components/messages/MessageForm.vue'
-import messagesApi from 'api/messages'
 
 export default {
-  props: ['messages'],
   components: {
     MessageRow,
     MessageForm
@@ -26,21 +23,10 @@ export default {
       message: null
     }
   },
-  computed: {
-    sortedMessages() {
-      return this.messages.sort((a, b) => -(a.id - b.id))
-    }
-  },
+  computed: mapGetters(['sortedMessages']),
   methods: {
     editMessage(message) {
       this.message = message
-    },
-    deleteMessage(message) {
-      messagesApi.remove(message.id).then(result => {
-        if (result.ok) {
-          this.messages.splice(this.messages.indexOf(message), 1)
-        }
-      })
     }
   }
 }
